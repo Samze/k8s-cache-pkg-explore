9e4d1911627604026dbdf55090746fbc18711884
Sam Gunaratne <sgunaratne@vmware.com>
2021-09-15 11:06:35 +0000

    Step 3: List with shared informer

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/samze/k8s-cache-pkg-exploer
 go 1.16
 
 require (
+	github.com/vmware-labs/service-bindings v0.4.0
+	k8s.io/apimachinery v0.22.1
 	k8s.io/client-go v0.22.1
+	knative.dev/pkg v0.0.0-20210915055909-d8349b0909c4
 	sigs.k8s.io/controller-runtime v0.10.0
 )
--- main.go
+++ main.go
@@ -3,37 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamiclister"
 	"k8s.io/client-go/tools/cache"
 )
 
 func main() {
-
 	// Create client
 	restCfg := ctrl.GetConfigOrDie()
 	dynamicClient, err := dynamic.NewForConfig(restCfg)
 	if err != nil {
 		panic(err)
 	}
-	gvk := schema.GroupVersionResource{Group: "rabbitmq.com", Version: "v1beta1", Resource: "rabbitmqclusters"}
-	client := dynamicClient.Resource(gvk)
-
-	res, err := client.List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("listing resources %+v", res)
+	gvr := schema.GroupVersionResource{Group: "rabbitmq.com", Version: "v1beta1", Resource: "rabbitmqclusters"}
+	client := dynamicClient.Resource(gvr)
 
 	// Create a list/watcher
-
-	lw := cache.ListWatch{
+	lw := &cache.ListWatch{
 		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
 			return client.List(context.TODO(), options)
 		},
@@ -42,9 +38,38 @@ func main() {
 		},
 	}
 
-	obj, err := lw.List(metav1.ListOptions{})
+	// Created Shared Informer
+	sharedInformer := cache.NewSharedIndexInformer(
+		lw,
+		&unstructured.Unstructured{},
+		time.Minute*20,
+		cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc},
+	)
+
+	stopChan := make(chan struct{})
+	go sharedInformer.Run(stopChan)
+	cache.WaitForCacheSync(stopChan, sharedInformer.HasSynced)
+
+	lister := dynamiclister.NewRuntimeObjectShim(dynamiclister.New(sharedInformer.GetIndexer(), gvr))
+
+	objs, err := lister.List(labels.NewSelector())
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("listing resources using listwatcher %+v", obj)
+	fmt.Printf("list using created sharedInformer %+v\n", objs)
+
+	sharedInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			fmt.Printf("event add %+v", obj)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			fmt.Printf("event update %+v", newObj)
+
+		},
+		DeleteFunc: func(obj interface{}) {
+			fmt.Printf("event delete %+v", obj)
+		},
+	})
+
+	time.Sleep(time.Minute)
 }
