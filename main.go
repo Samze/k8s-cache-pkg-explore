224334f1bb289f454a32ee8a9e475bbf394186ac
Sam Gunaratne <sgunaratne@vmware.com>
2021-09-15 10:53:14 +0000

    Step 2: Add listwatcher

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/cache"
 )
 
 func main() {
@@ -22,9 +25,26 @@ func main() {
 	gvk := schema.GroupVersionResource{Group: "rabbitmq.com", Version: "v1beta1", Resource: "rabbitmqclusters"}
 	client := dynamicClient.Resource(gvk)
 
-	res, err := client.List(context.TODO(), v1.ListOptions{})
+	res, err := client.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("listing resources %+v", res)
+
+	// Create a list/watcher
+
+	lw := cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			return client.List(context.TODO(), options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return client.Watch(context.TODO(), options)
+		},
+	}
+
+	obj, err := lw.List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("listing resources using listwatcher %+v", obj)
 }
