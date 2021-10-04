633a7f642d39a102cdbfae82c9f4e6f0c4ec9b28
Sam Gunaratne <sgunaratne@vmware.com>
2021-10-04 16:15:39 +0000

    Step 4: Informer factories

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"time"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/dynamic/dynamiclister"
+	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -26,39 +21,22 @@ func main() {
 		panic(err)
 	}
 	gvr := schema.GroupVersionResource{Group: "rabbitmq.com", Version: "v1beta1", Resource: "rabbitmqclusters"}
-	client := dynamicClient.Resource(gvr)
 
-	// Create a list/watcher
-	lw := &cache.ListWatch{
-		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-			return client.List(context.TODO(), options)
-		},
-		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-			return client.Watch(context.TODO(), options)
-		},
-	}
+	informerFact := dynamicinformer.NewDynamicSharedInformerFactory(dynamicClient, time.Minute*20)
 
-	// Created Shared Informer
-	sharedInformer := cache.NewSharedIndexInformer(
-		lw,
-		&unstructured.Unstructured{},
-		time.Minute*20,
-		cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc},
-	)
+	informer := informerFact.ForResource(gvr)
 
 	stopChan := make(chan struct{})
-	go sharedInformer.Run(stopChan)
-	cache.WaitForCacheSync(stopChan, sharedInformer.HasSynced)
+	go informerFact.Start(stopChan)
+	informerFact.WaitForCacheSync(stopChan)
 
-	lister := dynamiclister.NewRuntimeObjectShim(dynamiclister.New(sharedInformer.GetIndexer(), gvr))
-
-	objs, err := lister.List(labels.NewSelector())
+	objs, err := informer.Lister().List(labels.NewSelector())
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("list using created sharedInformer %+v\n", objs)
 
-	sharedInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			fmt.Printf("event add %+v", obj)
 		},
@@ -71,5 +49,6 @@ func main() {
 		},
 	})
 
-	time.Sleep(time.Minute)
+	//run forever
+	<-stopChan
 }
